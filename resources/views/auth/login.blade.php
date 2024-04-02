941b4dc299a83744fb426a1b5a5cc8f713b68316
Nickberms <s.bermejo.kein@cmu.edu.ph>
2024-04-02 15:27:19 +0800

    head head

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,56 +1,76 @@
 @extends('layouts.guest')
 @section('content')
-<div class="card-body login-card-body" style="font-family: Roboto, sans-serif">
-    <p class="login-box-msg" style="font-size: 20px;">{{ __('Login') }}</p>
-    <form action="{{ route('login') }}" method="post">
-        @csrf
-        <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                placeholder="{{ __('Email') }}" required autofocus>
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
+<html>
+
+<head>
+    <title>Login</title>
+    <link rel="stylesheet" href="admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="admin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="admin/plugins/toastr/toastr.min.css">
+    <link rel="icon" href="images/cmu_press_logo.png" type="image/png">
+    <script src="admin/plugins/sweetalert2/sweetalert2.min.js"></script>
+    <script src="admin/plugins/toastr/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body style="font-family: Roboto, sans-serif">
+    <div class="card-body login-card-body">
+        <p class="login-box-msg" style="font-size: 20px;">{{ __('Login') }}</p>
+        <form action="{{ route('login') }}" method="post">
+            @csrf
+            <div class="input-group mb-3">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                    placeholder="{{ __('Email') }}" required autofocus>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+                @error('email')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
+            </div>
+            <div class="input-group mb-3">
+                <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                    placeholder="{{ __('Password') }}" required>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-lock"></span>
+                    </div>
+                </div>
+                @error('password')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
+            </div>
+            <div class="row">
+                <div class="col-8">
+                    <div class="icheck-primary">
+                        <input type="checkbox" id="remember" name="remember">
+                        <label for="remember">
+                            {{ __('Remember Me') }}
+                        </label>
+                    </div>
+                </div>
+                <div class="col-4">
+                    <button type="submit" class="btn btn-success btn-block"
+                        style="background-color: #00491E; border-color: #00491E;">{{ __('Login') }}</button>
                 </div>
             </div>
-            @error('email')
-            <span class="error invalid-feedback">
-                {{ $message }}
-            </span>
-            @enderror
-        </div>
-        <div class="input-group mb-3">
-            <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
-                placeholder="{{ __('Password') }}" required>
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-lock"></span>
-                </div>
-            </div>
-            @error('password')
-            <span class="error invalid-feedback">
-                {{ $message }}
-            </span>
-            @enderror
-        </div>
-        <div class="row">
-            <div class="col-8">
-                <div class="icheck-primary">
-                    <input type="checkbox" id="remember" name="remember">
-                    <label for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-            </div>
-            <div class="col-4">
-                <button type="submit" class="btn btn-success btn-block"
-                    style="background-color: #00491E; border-color: #00491E;">{{ __('Login') }}</button>
-            </div>
-        </div>
-    </form>
-    @if (Route::has('password.request'))
-    <p class="mb-1">
-        <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
-    </p>
-    @endif
-</div>
+        </form>
+        @if (Route::has('password.request'))
+        <p class="mb-1">
+            <a href="{{ route('password.request') }}">{{ __('Forgot Your Password?') }}</a>
+        </p>
+        @endif
+    </div>
+</body>
+
+</html>
 @endsection
--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,34 +1,54 @@
 @extends('layouts.guest')
 @section('content')
-<div class="card-body login-card-body" style="font-family: Roboto, sans-serif">
-    <p class="login-box-msg" style="font-size: 20px;">{{ __('Reset Password') }}</p>
-    @if (session('status'))
-    <div class="alert alert-success" role="alert">
-        {{ session('status') }}
-    </div>
-    @endif
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
-        <div class="input-group mb-3">
-            <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
-                placeholder="{{ __('Email') }}" required autofocus>
-            <div class="input-group-append">
-                <div class="input-group-text">
-                    <span class="fas fa-envelope"></span>
+<html>
+
+<head>
+    <title>Reset Password</title>
+    <link rel="stylesheet" href="admin/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="admin/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+    <link rel="stylesheet" href="admin/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="admin/plugins/toastr/toastr.min.css">
+    <link rel="icon" href="images/cmu_press_logo.png" type="image/png">
+    <script src="admin/plugins/sweetalert2/sweetalert2.min.js"></script>
+    <script src="admin/plugins/toastr/toastr.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+</head>
+
+<body style="font-family: Roboto, sans-serif">
+    <div class="card-body login-card-body">
+        <p class="login-box-msg" style="font-size: 20px;">{{ __('Reset Password') }}</p>
+        @if (session('status'))
+        <div class="alert alert-success" role="alert">
+            {{ session('status') }}
+        </div>
+        @endif
+        <form method="POST" action="{{ route('password.email') }}">
+            @csrf
+            <div class="input-group mb-3">
+                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                    placeholder="{{ __('Email') }}" required autofocus>
+                <div class="input-group-append">
+                    <div class="input-group-text">
+                        <span class="fas fa-envelope"></span>
+                    </div>
+                </div>
+                @error('email')
+                <span class="error invalid-feedback">
+                    {{ $message }}
+                </span>
+                @enderror
+            </div>
+            <div class="row">
+                <div class="col-12">
+                    <button type="submit" class="btn btn-primary btn-block"
+                        style="background-color: #00491E; border-color: #00491E;">{{ __('Send Password Reset Link') }}</button>
                 </div>
             </div>
-            @error('email')
-            <span class="error invalid-feedback">
-                {{ $message }}
-            </span>
-            @enderror
-        </div>
-        <div class="row">
-            <div class="col-12">
-                <button type="submit" class="btn btn-primary btn-block"
-                    style="background-color: #00491E; border-color: #00491E;">{{ __('Send Password Reset Link') }}</button>
-            </div>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
+</body>
+
+</html>
 @endsection
